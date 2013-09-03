4f8791d7ae59a17a1f2c7718ffc24b03e5f53b10
Lars Olesen <lars@intraface.dk>
2013-09-03 16:39:10 +0200

    Small bugfixes to make the block work

--- js/donations_thermometer.js
+++ js/donations_thermometer.js
@@ -6,14 +6,18 @@
         targetHeight : 0,
         progressMeter : new Object(),
 
-
-        init : function(){
-          if(!document.getElementById) return false;
+        init : function() {
+          if (!document.getElementById) {
+            return false;
+          }
           this.resetValue();
           this.animateGauge();
         },
 
-        resetValue : function(){
+        resetValue : function() {
+          if (document.getElementById("campaign-progress-current"))) {
+            return false;
+          }
           this.progressMeter = document.getElementById("campaign-progress-current");
           this.targetHeight = this.progressMeter.offsetHeight;
           this.progressMeter.style.height = "0px";
@@ -22,10 +26,10 @@
         animateGauge : function(){
           var currHeight = this.progressMeter.offsetHeight;
 
-          if(currHeight == this.targetHeight){
+          if (currHeight == this.targetHeight){
           }
 
-          else{
+          else {
             var interval = Math.ceil((this.targetHeight - currHeight) / 10);
             this.progressMeter.style.height = currHeight + interval + "px";
             setTimeout("jQuery.gauge.animateGauge()",30);
