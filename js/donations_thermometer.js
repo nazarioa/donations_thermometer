23144fad7fd4611d5b41856bb6b431973ca36d7a
Nazario A. Ayala <nazario@niztech.com>
2015-11-05 11:57:55 -0800

    added ability to render with and height

--- js/donations_thermometer.js
+++ js/donations_thermometer.js
@@ -1,44 +1,64 @@
+"use strict";
 (function($){
   Drupal.behaviors.donations_thermometer = {
     attach: function(context, settings) {
+      var BLOCK_ID = Drupal.settings.donations_thermometer.block_number;
+      var ORIENTATION = Drupal.settings.donations_thermometer.block_number;
+
       $.gauge = {
 
-        targetHeight : 0,
+        goalHeight : 0,
+        goalWidth : 0,
         progressMeter : new Object(),
 
         init : function() {
-          if (!document.getElementById) {
+          if (!$('#donations_thermometer-' + BLOCK_ID)) {
             return false;
           }
+          console.log('Made it here: ' + BLOCK_ID);
           this.resetValue();
           this.animateGauge();
         },
 
         resetValue : function() {
-          if (document.getElementById("campaign-progress-current"))) {
+          if (!$('#donations_thermometer-' + BLOCK_ID)) {
             return false;
           }
-          this.progressMeter = document.getElementById("campaign-progress-current");
-          this.targetHeight = this.progressMeter.offsetHeight;
-          this.progressMeter.style.height = "0px";
+          this.progressMeter = $('#donations_thermometer-' + BLOCK_ID + ' .current-value');
+          this.goalHeight = this.progressMeter.outerHeight();
+          this.goalWidth = this.progressMeter.outerWidth();
+          this.progressMeter.css('height', 0);
         },
 
         animateGauge : function(){
-          var currHeight = this.progressMeter.offsetHeight;
-
-          if (currHeight == this.targetHeight){
+          if (!$('#donations_thermometer-' + BLOCK_ID)) {
+            return false;
           }
+          if(ORIENTATION === 'vertical'){
+            var currHeight = this.progressMeter.outerHeight();
 
-          else {
-            var interval = Math.ceil((this.targetHeight - currHeight) / 10);
-            this.progressMeter.style.height = currHeight + interval + "px";
-            setTimeout("jQuery.gauge.animateGauge()",30);
+            if (currHeight < this.goalHeight){
+              //animate growth
+              var interval = Math.ceil((this.goalHeight - currHeight) / 10);
+              this.progressMeter.css('height', (currHeight + interval) );
+              setTimeout("jQuery.gauge.animateGauge()",30);
+            }
+          }
+          else if (ORIENTATION === 'horizontal') {
+            var currWidth = this.progressMeter.outerWidth();
+
+            if (currWidth < this.goalWidth){
+              //animate growth
+              var interval = Math.ceil((this.goalWidth - currWidth) / 10);
+              this.progressMeter.css('width', (currWidth + interval) );
+              setTimeout("jQuery.gauge.animateGauge()",30);
+            }
           }
         }
       };
-      
+
       $.gauge.init();
     }
-    
+
   };
 })(jQuery);
--- templates/donations-thermometer.tpl.php
+++ templates/donations-thermometer.tpl.php
@@ -1,13 +1,15 @@
-  <div class="gauge-<?php print $size ?>">
-    <div class="current-value" id="campaign-progress-current" style="height: <?php print $percent; ?>%;">
-      <p><?php print $percent; ?>% </p>
+  <div id="donations_thermometer-<?php print $block_id ?>">
+      <div class="gauge-<?php print $size ?> <?php print $orientation ?> ">
+      <div class="current-value" style="height: <?php print $percent; ?>%;">
+        <p><?php print $percent; ?>% </p>
+      </div>
     </div>
+    <p>
+      <label><?php print t('Current:'); ?></label>
+      <span class="donations_thermometer-amount"><?php print $prefix; ?><?php print $current; ?><?php print $postfix; ?></span>
+    </p>
+    <p>
+      <label><?php print t('Goal:'); ?></label>
+      <span class="donations_thermometer-amount"><?php print $prefix; ?><?php print $goal; ?><?php print $postfix; ?></span>
+    </p>
   </div>
-  <p>
-    <span class="donations_thermometer-label"><?php print t('Current:'); ?></span>
-    <span class="donations_thermometer-amount"><?php print $prefix; ?><?php print $current; ?><?php print $postfix; ?></span>
-  </p>
-  <p>
-    <span class="donations_thermometer-label"><?php print t('Target:'); ?></span>
-    <span class="donations_thermometer-amount"><?php print $prefix; ?><?php print $target; ?><?php print $postfix; ?></span>
-  </p>
