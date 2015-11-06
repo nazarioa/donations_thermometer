1b94c7ab98107f210e92918ce4093d9e8fe09cf0
Nazario A. Ayala <nazario@niztech.com>
2015-11-06 10:57:01 -0800

    Fixed some styling issues. sisizng issues

--- templates/donations-thermometer.tpl.php
+++ templates/donations-thermometer.tpl.php
@@ -1,15 +1,15 @@
-  <div id="donations_thermometer-<?php print $block_id ?>">
-      <div class="gauge-<?php print $size ?> <?php print $orientation ?> ">
-      <div class="current-value" style="height: <?php print $percent; ?>%;">
-        <p><?php print $percent; ?>% </p>
-      </div>
-    </div>
-    <p>
-      <label><?php print t('Current:'); ?></label>
-      <span class="donations_thermometer-amount"><?php print $prefix; ?><?php print $current; ?><?php print $postfix; ?></span>
-    </p>
-    <p>
-      <label><?php print t('Goal:'); ?></label>
-      <span class="donations_thermometer-amount"><?php print $prefix; ?><?php print $goal; ?><?php print $postfix; ?></span>
-    </p>
+<div id="donations_thermometer-<?php print $block_id ?>">
+  <div class="gauge <?php print $size ?> <?php print $orientation ?> red">
+    <span class="current-value" style="<?php print $inlinecss ?>">
+      <!-- <p><?php print $percent; ?>% </p> -->
+    </span>
   </div>
+  <p>
+    <label><?php print t('Current:'); ?></label>
+    <span class="donations_thermometer-amount"><?php print $prefix; ?><?php print $current; ?><?php print $postfix; ?></span>
+  </p>
+  <p>
+    <label><?php print t('Goal:'); ?></label>
+    <span class="donations_thermometer-amount"><?php print $prefix; ?><?php print $goal; ?><?php print $postfix; ?></span>
+  </p>
+</div>
