d52ef686883f598f35dd3079aa4cdb895e3a57d5
Nazario A. Ayala <nazario@niztech.com>
2015-11-08 23:08:33 -0800

    Cleaned up code to comply with Drupal.org coding standards - Added @doctype for tpl.php file. - Added @file documentation to the CSS file.

--- templates/donations-thermometer.tpl.php
+++ templates/donations-thermometer.tpl.php
@@ -1,3 +1,26 @@
+<?php
+/**
+ * @file
+ * Donations Thermometer base output code.
+ *
+ * Available variables:
+ * - $block_id (int): Contains the ID number of this block.
+ * - $size (string): Size of the the thermometer used in CSS: 'small', 'large'
+ * - $orientation (string): Orientation of the the thermometer used in CSS:
+ * 'vertical', 'horizontal'
+ * - $inlinecss (string): CSS that contins the size of the thermometer.
+ * - $percent (double): percentage of the thermometer.
+ * - $goal (double): Thermometer goal.
+ * - $current (double): Current thermometer value.
+ * - $prefix (string): Prefix to 'Current' and 'Goal'.
+ * - $postfix (string): Prefix to 'Current' and 'Goal'.
+ *
+ * @see template_preprocess_forum_list()
+ *
+ * @ingroup themeable
+ */
+ ?>
+
 <div id="donations_thermometer-<?php print $block_id ?>">
   <div class="gauge <?php print $size ?> <?php print $orientation ?> red">
     <span class="current-value" style="<?php print $inlinecss ?>">
